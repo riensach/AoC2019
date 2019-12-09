984cce57a70a6f3db9575d1f378d0f040b6f20d5
Rien <rien.sach@btinternet.com>
2019-12-09 21:42:18 +0000

    More updates!

---
+++ AoCDay9b.php
@@ -0,0 +1,230 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$inputData = ("1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1101,0,3,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1101,0,36,1015,1102,1,387,1028,1101,24,0,1016,1101,0,23,1008,1102,1,35,1012,1102,1,554,1023,1101,29,0,1003,1101,27,0,1011,1101,25,0,1000,1101,0,38,1018,1102,20,1,1019,1102,28,1,1005,1102,1,619,1026,1102,1,22,1004,1101,0,0,1020,1101,0,31,1009,1102,1,783,1024,1102,1,33,1001,1102,616,1,1027,1102,1,21,1006,1101,32,0,1013,1102,39,1,1014,1102,1,378,1029,1101,774,0,1025,1102,1,1,1021,1102,30,1,1007,1102,37,1,1002,1102,1,26,1017,1101,0,557,1022,1102,1,34,1010,109,13,2101,0,-5,63,1008,63,23,63,1005,63,203,4,187,1105,1,207,1001,64,1,64,1002,64,2,64,109,-14,2107,28,4,63,1005,63,225,4,213,1106,0,229,1001,64,1,64,1002,64,2,64,109,10,1207,-3,20,63,1005,63,245,1106,0,251,4,235,1001,64,1,64,1002,64,2,64,109,8,1205,3,263,1105,1,269,4,257,1001,64,1,64,1002,64,2,64,109,-9,1207,-7,34,63,1005,63,287,4,275,1105,1,291,1001,64,1,64,1002,64,2,64,109,-4,2102,1,-3,63,1008,63,32,63,1005,63,311,1105,1,317,4,297,1001,64,1,64,1002,64,2,64,109,21,21101,40,0,-6,1008,1019,43,63,1005,63,337,1106,0,343,4,323,1001,64,1,64,1002,64,2,64,109,-26,1202,7,1,63,1008,63,21,63,1005,63,365,4,349,1106,0,369,1001,64,1,64,1002,64,2,64,109,26,2106,0,3,4,375,1001,64,1,64,1105,1,387,1002,64,2,64,109,-9,21108,41,40,3,1005,1019,407,1001,64,1,64,1106,0,409,4,393,1002,64,2,64,109,13,1205,-8,423,4,415,1106,0,427,1001,64,1,64,1002,64,2,64,109,-19,21107,42,41,5,1005,1015,447,1001,64,1,64,1106,0,449,4,433,1002,64,2,64,109,-3,2102,1,-5,63,1008,63,37,63,1005,63,471,4,455,1105,1,475,1001,64,1,64,1002,64,2,64,109,-2,1201,0,0,63,1008,63,28,63,1005,63,497,4,481,1105,1,501,1001,64,1,64,1002,64,2,64,109,8,2107,29,-8,63,1005,63,521,1001,64,1,64,1106,0,523,4,507,1002,64,2,64,109,-3,1208,-3,30,63,1005,63,541,4,529,1106,0,545,1001,64,1,64,1002,64,2,64,109,4,2105,1,9,1105,1,563,4,551,1001,64,1,64,1002,64,2,64,109,9,1206,-3,581,4,569,1001,64,1,64,1106,0,581,1002,64,2,64,109,-8,1201,-9,0,63,1008,63,23,63,1005,63,605,1001,64,1,64,1106,0,607,4,587,1002,64,2,64,109,21,2106,0,-9,1106,0,625,4,613,1001,64,1,64,1002,64,2,64,109,-35,2108,31,8,63,1005,63,647,4,631,1001,64,1,64,1105,1,647,1002,64,2,64,109,2,1202,0,1,63,1008,63,30,63,1005,63,667,1105,1,673,4,653,1001,64,1,64,1002,64,2,64,109,17,21108,43,43,-4,1005,1016,691,4,679,1106,0,695,1001,64,1,64,1002,64,2,64,109,-14,1208,-1,30,63,1005,63,711,1106,0,717,4,701,1001,64,1,64,1002,64,2,64,109,6,21101,44,0,-1,1008,1011,44,63,1005,63,739,4,723,1105,1,743,1001,64,1,64,1002,64,2,64,109,-15,2108,30,8,63,1005,63,759,1106,0,765,4,749,1001,64,1,64,1002,64,2,64,109,27,2105,1,0,4,771,1001,64,1,64,1105,1,783,1002,64,2,64,109,-9,1206,6,795,1105,1,801,4,789,1001,64,1,64,1002,64,2,64,109,4,21102,45,1,-7,1008,1012,45,63,1005,63,823,4,807,1105,1,827,1001,64,1,64,1002,64,2,64,109,-14,21102,46,1,5,1008,1010,43,63,1005,63,851,1001,64,1,64,1105,1,853,4,833,1002,64,2,64,109,-1,2101,0,1,63,1008,63,25,63,1005,63,873,1105,1,879,4,859,1001,64,1,64,1002,64,2,64,109,9,21107,47,48,-3,1005,1010,897,4,885,1105,1,901,1001,64,1,64,4,64,99,21101,0,27,1,21101,915,0,0,1106,0,922,21201,1,57526,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21101,942,0,0,1106,0,922,21201,1,0,-1,21201,-2,-3,1,21101,957,0,0,1106,0,922,22201,1,-1,-2,1105,1,968,21202,-2,1,-2,109,-3,2106,0,0");
+
+//$inputData = ("1002,4,3,4,33");
+
+//$inputData = ("3,0,4,0,99");
+//
+//
+//$inputData = ("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10");
+//$inputData = ("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99");
+//$inputData = ("1102,34915192,34915192,7,4,7,99,0");
+//$inputData = ("104,1125899906842624,99");
+$arrayInfo = explode(",",$inputData);
+
+
+$arrayLength = count($arrayInfo);
+
+        
+        $array = implode(",", $arrayInfo);
+        //print_r($array);
+        //echo "<br>";
+        
+
+                    $processor1 = new processCode(0, $arrayInfo, 2);
+                    $processor1->processCodeFunction();
+                    $outputValue1 = $processor1->output;
+
+   //$outputValue = processCode(0,$arrayInfo);
+    
+   echo "Output: $outputValue1";
+           
+        
+ class processCode {  
+
+    public $output;
+    public $inputCode;
+    public $phaseCode;
+    public $processorMemory;
+    public $processorPosition;
+    public $processorID;
+    public $halted = 0;
+    public $inputValueRequest = 0;
+    public $relativeBase = 0;
+    public $inputValue;
+    public $positionMode1;
+    public $positionMode2;
+    public $positionMode3;
+    public $operation;
+    
+    function __construct($phaseCode, $processorMemory, $processorID) {
+        $this->phaseCode = $phaseCode;
+        $this->processorMemory = $processorMemory;
+        $this->processorPosition = 0;
+        $this->processorID = $processorID;
+       // echo "<br>Starting processing. With position:".$this->inputCode."<br>";
+    }
+    
+    function updateInput($inputCode) {
+        $this->inputCode = $inputCode;
+    }
+    
+    function getReference($positionMode,$offset) {        
+        $arrayLength = count($this->processorMemory);
+        if($positionMode==1) {
+            $value = $this->processorMemory[$this->processorPosition+$offset];
+        } elseif($positionMode==2) {
+            $positionValue = $this->relativeBase + $this->processorMemory[$this->processorPosition+$offset];
+            $value = $this->processorMemory[$positionValue];
+        } else {
+            $positionValue = $this->processorMemory[$this->processorPosition+$offset];
+            $value = $this->processorMemory[$positionValue];            
+        }
+        return $value;
+    }
+    
+    function getSaveReference($positionMode,$offset) {        
+        $arrayLength = count($this->processorMemory);
+        if($positionMode==1) {
+            $positionValue = $this->processorMemory[$this->processorPosition+$offset];
+        } elseif($positionMode==2) {
+            $positionValue = $this->relativeBase + $this->processorMemory[$this->processorPosition+$offset];
+        } else {
+            $positionValue = $this->processorMemory[$this->processorPosition+$offset];      
+        }
+        return $positionValue;    
+    }
+    
+    function updateInputReference() {
+        if($this->inputValueRequest > 0) {
+            $this->inputValue = $this->inputCode;
+        } 
+        if($this->inputValueRequest == 1 && $this->processorID == 1) {
+            $this->inputValue = 0;
+        } 
+    }
+    
+    function processOperation() {        
+        unset($this->positionMode1,$this->positionMode2,$this->positionMode3);
+        
+        if(strlen($this->operation)>1) {
+            if(strlen($this->operation)>2) {
+                $this->positionMode1 = substr($this->operation,-3,1);
+            }
+
+            if(strlen($this->operation)>3) {
+                $this->positionMode2 = substr($this->operation,-4,1);
+            }
+
+            if(strlen($this->operation)>4) {
+                $this->positionMode3 = substr($this->operation,-5,1);
+            }
+            $this->operation = substr($this->operation,-2);        
+        }
+        if(!isset($this->positionMode1)) { $this->positionMode1 = 0; }
+        if(!isset($this->positionMode2)){ $this->positionMode2 = 0; }
+        if(!isset($this->positionMode3)){ $this->positionMode3 = 0; }
+    }
+    
+    public function processCodeFunction() {
+        $this->inputValue = 2;
+        $this->updateInputReference();
+        $exit = 0;
+
+        if($this->halted == 1) {
+            return;
+        }
+        while($exit < 1) {
+            $this->operation = $this->processorMemory[$this->processorPosition];
+            $this->processOperation();
+
+            if($this->operation == 1) {
+                // Add 1 + 2 and store in 3
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);
+                $result = $value1 + $value2;
+                $value3 = $this->getSaveReference($this->positionMode3,3);
+                $this->processorMemory[$value3] = $result;
+                $this->processorPosition = $this->processorPosition + 4;
+            } elseif($this->operation == 2) {
+                // Multiply 1 + 2 and store in 3
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);
+                $result = $value1 * $value2;
+                $value3 = $this->getSaveReference($this->positionMode3,3);
+                $this->processorMemory[$value3] = $result;
+                $this->processorPosition = $this->processorPosition + 4;
+            } elseif($this->operation == 3) {
+                // Save input into position
+                $value3 = $this->getSaveReference($this->positionMode1,1);
+                $this->processorMemory[$value3] = $this->inputValue;
+                $this->processorPosition = $this->processorPosition + 2;
+                $this->inputValueRequest = $this->inputValueRequest + 1;
+                $this->updateInputReference();
+            } elseif($this->operation == 4) {
+                $value1 = $this->getReference($this->positionMode1,1); 
+                $output = $value1;              
+                $this->output = $output;
+                $this->processorPosition = $this->processorPosition + 2;
+                $exit = 1;
+            } elseif($this->operation == 5) {
+                // Jump if true        
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);
+                if($value1<>0) {                    
+                    $this->processorPosition = $value2; 
+                } else {
+                    $this->processorPosition = $this->processorPosition + 3;
+                }
+            } elseif($this->operation == 6) {
+                // Jump if false
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);                
+                if($value1==0) {                    
+                    $this->processorPosition = $value2; 
+                } else {
+                    $this->processorPosition = $this->processorPosition + 3;
+                }
+            } elseif($this->operation == 7) {
+                // Store 1 if less than
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);
+
+                $storedValue = 0;
+                if($value1 < $value2) {
+                    $storedValue = 1;
+                }
+                
+                $value3 = $this->getSaveReference($this->positionMode3,3);
+                $this->processorMemory[$value3] = $storedValue;
+                $this->processorPosition = $this->processorPosition + 4;
+
+            } elseif($this->operation == 8) {
+                // Store 1 if equal
+                $value1 = $this->getReference($this->positionMode1,1);                
+                $value2 = $this->getReference($this->positionMode2,2);
+
+                $storedValue = 0;
+                if($value1 == $value2) {
+                    $storedValue = 1;
+                }
+                $value3 = $this->getSaveReference($this->positionMode3,3);
+                $this->processorMemory[$value3] = $storedValue;
+                $this->processorPosition = $this->processorPosition + 4;
+
+            } elseif($this->operation == 9) {
+                // Adjust the relative base                
+                $value1 = $this->getReference($this->positionMode1,1); 
+                $this->relativeBase = $this->relativeBase + $value1;
+                $this->processorPosition = $this->processorPosition + 2;
+            } elseif($this->operation == 99) {
+                // Halt
+                $exit = 1;
+                $this->halted = 1;
+            } else {
+                //echo "catch all <br>";
+            }
+
+        } 
+
+    }
+    
+ }
+
